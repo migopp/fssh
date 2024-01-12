060a6eaf4959cb268fb5238b8685e148de3f342c
Michael Goppert <goppert@utexas.edu>
2024-01-12 03:34:41 -0600

    Fix fssh-setup shell path

--- src/fssh/setup.py
+++ src/fssh/setup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 import subprocess
+import os
 
 def setup():
-    subprocess.run(['chmod', '+x', 'src/fssh/setup.sh'])
-    subprocess.run('./src/fssh/setup.sh')
+    path = os.path.abspath(__file__)
+    script_path = f'{path[:len(path) - 2]}sh'
+    subprocess.run(['chmod', '+x', script_path])
+    subprocess.run(script_path)
 
 if __name__ == '__main__':
     setup()
